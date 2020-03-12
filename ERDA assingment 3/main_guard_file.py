5c9fba834e8b7a21185dbca8916d074f318e6f04
Johannes Algera <johannesbsb@hotmail.com>
2020-03-12 12:29:33 +0100

    many changes

--- ERDA assingment 3/main_guard_file.py
+++ ERDA assingment 3/main_guard_file.py
@@ -15,6 +15,10 @@ from random import *
 import matplotlib.pyplot as plt
 from wall_finder import vision
 from itertools import permutations
+from copy import deepcopy
+import itertools
+
+
 width= int(160/5)
 height= int(30/5)
 tabley_start= int(10/5)
@@ -22,7 +26,7 @@ tabley_end= int(20/5)
 tablespc=int(47/5)
 reso=5
 totalsteps=2
-size=(height*reso,width*reso)  #resolution= 10 cm
+size=(height*reso,width*reso)  #resolution= 50 cm
 
 gallery=np.zeros(size)   
 #Placing the walls
@@ -40,6 +44,20 @@ gallery[round(tabley_start*reso):round(tabley_end*reso),round((21/5+tablespc)*re
 gallery[round(tabley_start*reso):round(tabley_end*reso),round((21/5+2*tablespc)*reso):round((45/5+2*tablespc)*reso)]=-1
 
 #------------------------------------------------------------------------------
+#                           Functions setup
+#------------------------------------------------------------------------------
+def percentage(room,size):   #calculates the percentage of the room that is visible
+
+    viewedarea = room!= 0  # Here a Boolean matrix is returned where "True" means visible, wall, table or guard
+    viewedblocks = sum(sum(viewedarea))  # Sums the amount of visible 'blocks'
+    perc = viewedblocks / (size[0] * size[1]) * 100  # calculates % of viewed blocks
+    print(perc, "%", " of the room is visible.")
+    return perc
+
+
+
+
+#------------------------------------------------------------------------------
 #                           First Guard Placement
 #------------------------------------------------------------------------------
 
@@ -54,7 +72,7 @@ index2 = 0
 for guard in guards:
     value=gallery[guard[0],guard[1]]
     while value==1 or value==-1:
-        guard=[randint(0,size[0]),randint(0,size[1])]
+        guard=[randint(0,size[0]-1),randint(0,size[1]-1)]
         print("Can't place guard on wall or table")
         value=gallery[guard[0],guard[1]]
     gallery[guard[0],guard[1]]=4
@@ -62,101 +80,109 @@ for guard in guards:
     index2+=1
     
 
-#print(guards)
-#plt.imshow(gallery)
-#plt.show()
 
 #------------------------------------------------------------------------------
 #                           Placement iteration 
 #------------------------------------------------------------------------------
-itercount=0
-while itercount <= totalsteps:
- 
-    for guard in guards:            #Applying the viewing function from wall_finder.py                             
+
+#sets up a list of possible movements
+steps = ['00','11']
+poss=[['00','00','00','00'],['00','00','00','11'],['00','00','11','00'],['00','00','11','11'],['00','11','00','00'],['00','11','00','11'],['00','11','11','00'],['00','11','11','11'],['11','00','00','00'],['11','00','00','11'],['11','00','11','00'],['11','00','11','11'],['11','11','00','00'],['11','11','00','11'],['11','11','11','00'],['11','11','11','11'],]
+points = []
+
+itercount = 0
+previous_p = 0
+current_p = 0
+repeated = 0
+while True:  ###later change to percentage <100%
+
+    for guard in guards:            #Applying the viewing function from wall_finder.py
         gallery=vision(guard,gallery,1/reso)
         print()
         print(guard)
     plt.imshow(gallery)
     plt.show()
-    
-
-    viewedarea= gallery!=0          #Here a Boolean matrix is returned where "True" means visible, wall, table or guard
-    viewedblocks=sum(sum(viewedarea))  #Sums the amount of visible 'blocks'
-    perc= viewedblocks/(size[0]*size[1])*100    #calculates % of viewed blocks
-    print(perc,"%"," of the room is visible.")
 
 
-    
+    current_p = percentage(gallery,size)
+    if current_p>99.95:
+        break
+    #if previous_p>current_p:#if the move wasnt efective choose a new one and move the guards back
+    #    guards = previous_guards
+   #     moves = poss[repeated]
+   #     repeated += 1
+
+
+    previous_p = percentage(gallery,size)
+
+
     noguards= gallery < 4
     novis= gallery != 2   #'Resets' the gallery matrix
-    gallery= gallery*noguards*novis   
+    gallery= gallery*noguards*novis
 
-    if itercount <totalsteps:
-        steps=['00','01','11','01']
-        poss=list(permutations(steps))
-        moves=poss[randint(0,len(poss)-1)]
-        walkingcode1='01010101'
-        walkingcode2='00000000'
-        # n=2
-        # if itercount==0:        #This splits the walking code into the different steps for the different guards
-        #     moves=[walkingcode1[i:i+n] for i in range(0,len(walkingcode1),n)]
-        # elif itercount==1:
-        #     moves=[walkingcode2[i:i+n] for i in range(0,len(walkingcode2),n)]
-        print(moves)
-        i=0
-        step=1*reso
-        for move in moves:
-            if move=='01': #move left
-                a=guards[i][1]
-                a-=step
-                if a<0:
-                  a=1  
-                value= gallery[guards[i][0],a]
-                if value !=1 and value !=-1:
-                    guards[i][1]=a
-                else:
-                    print("Can't walk into a wall")
 
-            elif move== '10': #move right
-                a=guards[i][1]
-                a+=step
-                if a>(size[1]-1):
-                    a=size[1]-2
-                value= gallery[guards[i][0],a]
-                if value !=1 and value !=-1:
-                    guards[i][1]=a
-                else:
-                    print("Can't walk into a wall")
-                
-            elif move=='00':#move forward
-                a=guards[i][0]
-                a+=step
-                if a>(size[0]-1):
-                    a=size[0]-2
-                value= gallery[a,guards[i][1]]
-                if value !=1 and value !=-1:
-                    guards[i][0]=a
-                else:
-                    print("Can't walk into a wall")
-                
-            elif move=='11': #move backwards
-                a=guards[i][0]
-                a-=step
-                if a<0:
-                  a=1  
-                value= gallery[a,guards[i][1]]
-                if value !=1 and value !=-1:
-                    guards[i][0]=a
-                else:
-                    print("Can't walk into a wall")
-            i+=1
-            
     i=0
+    step=1*reso
+    j = 0
+
+    previous_guards = deepcopy(guards)
+    for moves in poss:
+
+        print("star movement positions")
+        print(guards)
+        for move in moves:
+                if move=='01': #move left
+                    a=guards[i][1]
+                    a-=step
+                    if a<0:
+                      a=1
+                    value= gallery[guards[i][0],a]
+                    if value !=1 and value !=-1:
+                        guards[i][1]=a
+                    else:
+                        print("Can't walk into a wall")
+
+                elif move== '10': #move right
+                    a=guards[i][1]
+                    a+=step
+                    if a>(size[1]-1):
+                        a=size[1]-2
+                    value= gallery[guards[i][0],a]
+                    if value !=1 and value !=-1:
+                        guards[i][1]=a
+                    else:
+                        print("Can't walk into a wall")
+
+                elif move=='00':#move forward
+                    a=guards[i][0]
+                    a+=step
+                    if a>(size[0]-1):
+                        a=size[0]-2
+                    value= gallery[a,guards[i][1]]
+                    if value !=1 and value !=-1:
+                        guards[i][0]=a
+                    else:
+                        print("Can't walk into a wall")
+
+                elif move=='11': #move backwards
+                    a=guards[i][0]
+                    a-=step
+                    if a<0:
+                      a=1
+                    value= gallery[a,guards[i][1]]
+                    if value !=1 and value !=-1:
+                        guards[i][0]=a
+                    else:
+                        print("Can't walk into a wall")
+                i+=1
+
+        i=0
+        j+=1
     for guard in guards:                    #Checking if the new guard placement doesn't place guards on walls or tables                                         
         gallery[guard[0],guard[1]]=4
         guards[i] = guard
         i+=1
-    itercount+=1
+
     print("New positions")
     print(guards)
 
--- ERDA assingment 3/wall_finder.py
+++ ERDA assingment 3/wall_finder.py
@@ -9,7 +9,7 @@ def vision(guard, room,reso_matrix):
     # 2 is a point that guards can see
     # it will start with polar coordinates and check for each angle how far the guard is from a wall
 
-    angle_reso = 1000  # number of points for angle
+    angle_reso = 500  # number of points for angle
     radius_reso = reso_matrix/15  #small increment for angle
     theta_list = np.linspace(0, 2 * np.pi, angle_reso) #list with angles
     r = 0
@@ -42,11 +42,3 @@ def vision(guard, room,reso_matrix):
 
     return room
 
-# guard = [1, 1]
-# room =np.array( [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-#        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
-#        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-#        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]])
-
-# print(vision(guard, room,0.4))
