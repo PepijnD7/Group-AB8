8a64c41d4da5576b891db4d3cee77c63eb7b28a9
lucaelbracht <30736626+lucaelbracht@users.noreply.github.com>
2020-03-26 13:34:24 +0100

    Update extension_time.py

--- code/extension_time.py
+++ code/extension_time.py
@@ -27,7 +27,7 @@ vcsl_4=list()
 data_file=open("Dis1.txt","r")
 i=0
 for line in data_file.readlines():
-    if i>1:
+    if i>0:
         char=line.split("\t")
         tl_1.append(float(char[0].strip()))
         vpsl_1.append(float(char[1].strip()))
@@ -38,7 +38,7 @@ data_file.close()
 data_file=open("Dis2.txt","r")
 i=0
 for line in data_file.readlines():
-    if i>1:
+    if i>0:
         char=line.split("\t")
         tl_2.append(float(char[0].strip()))
         vpsl_2.append(float(char[1].strip()))
@@ -49,7 +49,7 @@ data_file.close()
 data_file=open("Dis3.txt","r")
 i=0
 for line in data_file.readlines():
-    if i>1:
+    if i>0:
         char=line.split("\t")
         tl_3.append(float(char[0].strip()))
         vpsl_3.append(float(char[1].strip()))
@@ -60,7 +60,7 @@ data_file.close()
 data_file=open("Dis4.txt","r")
 i=0
 for line in data_file.readlines():
-    if i>1:
+    if i>0:
         char=line.split("\t")
         tl_4.append(float(char[0].strip()))
         vpsl_4.append(float(char[1].strip()))
@@ -88,12 +88,6 @@ dis2=10*vps_2/2
 dis3=10*vps_3/2
 dis4=10*vps_4/2
 
-plt.plot(t1,dis1,':')
-plt.plot(t2,dis2,'-.')
-plt.plot(t3,dis3,'--')
-plt.plot(t4,dis4,'-')
-plt.show()
-
 #calculate the current from vcs using the relation found in excel
 # current=(vcs-2.521)/(-0.1457)
 c1=(vcs_1-2.521)/(-0.1457)
@@ -111,44 +105,95 @@ f3=(c3-n)/dy_dx
 f4=(c4-n)/dy_dx
 
 #subtract error
+#find the force and displacement values corresponding to t=-1
 i=0
 fsum=0
-while t1[i]==-1:
-    fsum=fsum+f1[i]
-    i=i+1
-fsum=fsum+f1[i-1]
-error1=fsum/(i+1)
+dissum=0
+while t1[i] == -1:
+    dissum += dis1[i]
+    fsum += f1[i]
+    i += 1
+error1 = fsum/i
+errordis1 = dissum/i
+t1=np.delete(t1,np.s_[:i])
+f1=np.delete(f1,np.s_[:i])
+dis1=np.delete(dis1,np.s_[:i])
 
 i=0
 fsum=0
-while t2[i]==-1:
-    fsum=fsum+f2[i]
-    i=i+1
-    print(fsum)
-fsum=fsum+f2[i-1]
-print(fsum)
-error2=fsum/(i+1)
+dissum=0
+while t2[i]==-1: 
+    dissum += dis2[i]
+    fsum+=f2[i]
+    i+=1
+error2=fsum/i
+errordis2 = dissum/i
+t2=np.delete(t2,np.s_[:i])
+f2=np.delete(f2,np.s_[:i])
+dis2=np.delete(dis2,np.s_[:i])
 
 i=0
 fsum=0
+dissum=0
 while t3[i]==-1:
-    fsum=fsum+f3[i]
-    i=i+1
-fsum=fsum+f3[i-1]
-error3=fsum/(i+1)
+    dissum += dis3[i]
+    fsum+=f3[i]
+    i+=1
+error3=fsum/i
+errordis3 = dissum/i
+t3=np.delete(t3,np.s_[:i])
+f3=np.delete(f3,np.s_[:i])
+dis3=np.delete(dis3,np.s_[:i])
 
 i=0
 fsum=0
+dissum=0
 while t4[i]==-1:
-    fsum=fsum+f4[i]
-    i=i+1
-fsum=fsum+f4[i-1]
-error4=fsum/(i+1)
+    dissum += dis4[i]
+    fsum+=f4[i]
+    i+=1
+error4=fsum/i
+errordis4 = dissum/i
+t4=np.delete(t4,np.s_[:i])
+f4=np.delete(f4,np.s_[:i])
+dis4=np.delete(dis4,np.s_[:i])
 
-error=np.array([error1,error2,error3,error4])
+error=np.array([[error1,error2,error3,error4],
+               [errordis1,errordis2,errordis3,errordis4]])
 
-plt.plot(t1,f1,':')
-plt.plot(t2,f2,'-.')
-plt.plot(t3,f3,'--')
-plt.plot(t4,f4,'-')
+f1=f1-error[0,0]
+f2=f2-error[0,1]
+f3=f3-error[0,2]
+f4=f4-error[0,3]
+
+dis1=dis1-error[1,0]
+dis2=dis2-error[1,1]
+dis3=dis3-error[1,2]
+dis4=dis4-error[1,3]
+
+#set time to zero
+t1=t1-t1[0]
+t2=t2-t2[0]
+t3=t3-t3[0]
+t4=t4-t4[0]
+
+
+plt.grid()
+plt.ylabel('Actuator extension [mm]')
+plt.xlabel('Time [s]')
+plt.plot(t1,dis1,':',label='10mm')
+plt.plot(t2,dis2,'-.',label='20mm')
+plt.plot(t3,dis3,'--',label='30mm')
+plt.plot(t4,dis4,'-',label='39mm')
+plt.legend(title='Maximum extension:')
+plt.show()
+
+plt.grid()
+plt.ylabel('Actuation force [N]')
+plt.xlabel('Time [s]')
+plt.plot(t1,f1,':',label='10mm')
+plt.plot(t2,f2,'-.',label='20mm')
+plt.plot(t3,f3,'--',label='30mm')
+plt.plot(t4,f4,'-',label='39mm')
+plt.legend(title='Maximum extension:', loc='upper right')
 plt.show()
