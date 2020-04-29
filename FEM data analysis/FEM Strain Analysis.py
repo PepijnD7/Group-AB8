6cd1ae8bc92aa53bcd59ce9160bccdd85cbba20f
aiwainaah <62008500+aiwainaah@users.noreply.github.com>
2020-04-29 17:41:53 +0200

    Update FEM Strain Analysis.py

--- FEM data analysis/FEM Strain Analysis.py
+++ FEM data analysis/FEM Strain Analysis.py
@@ -139,8 +139,8 @@ for i in range(0, len(x)-1):
     T = np.arctan(abs(y[i]-y[i+1])/abs(x[i]-x[i+1]))
     theta.append(T)
 
-print(max(theta_list)) # Should be around pi/2 for the tip of the leading edge
-print(len(theta_list))
+print(max(theta)) # Should be around pi/2 for the tip of the leading edge
+print(len(theta))
 
 ########################################################################################################
 
@@ -156,6 +156,7 @@ str_in_x_1   = array_str1_in[:,1]
 str_out_y_1  = array_str1_out[:,2]
 str_in_y_1   = array_str1_in[:,2]
 str_out_z_1  = array_str1_out[:,3]
+str_in_z_1   = array_str1_in[:,3]
 str_out_xy_1 = array_str1_out[:,4]
 str_in_xy_1  = array_str1_in[:,4]
 str_out_yz_1 = array_str1_out[:,5]
@@ -169,6 +170,7 @@ str_in_x_2   = array_str2_in[:,1]
 str_out_y_2  = array_str2_out[:,2]
 str_in_y_2   = array_str2_in[:,2]
 str_out_z_2  = array_str2_out[:,3]
+str_in_z_2   = array_str2_in[:,3]
 str_out_xy_2 = array_str2_out[:,4]
 str_in_xy_2  = array_str2_in[:,4]
 str_out_yz_2 = array_str2_out[:,5]
@@ -182,6 +184,7 @@ str_in_x_3   = array_str3_in[:,1]
 str_out_y_3  = array_str3_out[:,2]
 str_in_y_3   = array_str3_in[:,2]
 str_out_z_3  = array_str3_out[:,3]
+str_in_z_3   = array_str3_in[:,3]
 str_out_xy_3 = array_str3_out[:,4]
 str_in_xy_3  = array_str3_in[:,4]
 str_out_yz_3 = array_str3_out[:,5]
@@ -195,6 +198,7 @@ str_in_x_4   = array_str4_in[:,1]
 str_out_y_4  = array_str4_out[:,2]
 str_in_y_4   = array_str4_in[:,2]
 str_out_z_4  = array_str4_out[:,3]
+str_in_z_4   = array_str4_in[:,3]
 str_out_xy_4 = array_str4_out[:,4]
 str_in_xy_4  = array_str4_in[:,4]
 str_out_yz_4 = array_str4_out[:,5]
@@ -208,6 +212,7 @@ str_in_x_5   = array_str5_in[:,1]
 str_out_y_5  = array_str5_out[:,2]
 str_in_y_5   = array_str5_in[:,2]
 str_out_z_5  = array_str5_out[:,3]
+str_in_z_5   = array_str5_in[:,3]
 str_out_xy_5 = array_str5_out[:,4]
 str_in_xy_5  = array_str5_in[:,4]
 str_out_yz_5 = array_str5_out[:,5]
@@ -225,40 +230,117 @@ str_in_xz_5  = array_str5_in[:,6]
 # Get the transformed strain and place them in lists for xprime and yprime, both on the inside and outside:
 str_xprime1_out = []
 str_xprime2_out = []
+str_xprime3_out = []
+str_xprime4_out = []
+str_xprime5_out = []
 
 str_xprime1_in = []
 str_xprime2_in = []
-
+str_xprime3_in = []
+str_xprime4_in = []
+str_xprime5_in = []
 
 str_yprime1_out = []
 str_yprime2_out = []
+str_yprime3_out = []
+str_yprime4_out = []
+str_yprime5_out = []
 
 str_yprime1_in = []
 str_yprime2_in = []
+str_yprime3_in = []
+str_yprime4_in = []
+str_yprime5_in = []
 
-for i in range(0,len(theta_list)):
+
+for i in range(0,len(theta)):
     xprime1_out = str_out_x_1[i]*(np.cos(theta[i]))**2 + str_out_y_1[i]*(np.sin(theta[i]))**2 + 2*str_out_xy_1[i]*np.sin(theta[i])*np.cos(theta[i])
     xprime2_out = str_out_x_2[i]*(np.cos(theta[i]))**2 + str_out_y_2[i]*(np.sin(theta[i]))**2 + 2*str_out_xy_2[i]*np.sin(theta[i])*np.cos(theta[i])
     xprime3_out = str_out_x_3[i]*(np.cos(theta[i]))**2 + str_out_y_3[i]*(np.sin(theta[i]))**2 + 2*str_out_xy_3[i]*np.sin(theta[i])*np.cos(theta[i])
     xprime4_out = str_out_x_4[i]*(np.cos(theta[i]))**2 + str_out_y_4[i]*(np.sin(theta[i]))**2 + 2*str_out_xy_4[i]*np.sin(theta[i])*np.cos(theta[i])
     xprime5_out = str_out_x_5[i]*(np.cos(theta[i]))**2 + str_out_y_5[i]*(np.sin(theta[i]))**2 + 2*str_out_xy_5[i]*np.sin(theta[i])*np.cos(theta[i])
 
-    xprime1_in = ...
-    xprime2_in = ...
-    ....
-
+    xprime1_in = str_in_x_1[i]*(np.cos(theta[i]))**2 + str_in_y_1[i]*(np.sin(theta[i]))**2 + 2*str_in_xy_1[i]*np.sin(theta[i])*np.cos(theta[i])
+    xprime2_in = str_in_x_2[i]*(np.cos(theta[i]))**2 + str_in_y_2[i]*(np.sin(theta[i]))**2 + 2*str_in_xy_2[i]*np.sin(theta[i])*np.cos(theta[i])
+    xprime3_in = str_in_x_3[i]*(np.cos(theta[i]))**2 + str_in_y_3[i]*(np.sin(theta[i]))**2 + 2*str_in_xy_3[i]*np.sin(theta[i])*np.cos(theta[i])
+    xprime4_in = str_in_x_4[i]*(np.cos(theta[i]))**2 + str_in_y_4[i]*(np.sin(theta[i]))**2 + 2*str_in_xy_4[i]*np.sin(theta[i])*np.cos(theta[i])
+    xprime5_in = str_in_x_5[i]*(np.cos(theta[i]))**2 + str_in_y_5[i]*(np.sin(theta[i]))**2 + 2*str_in_xy_5[i]*np.sin(theta[i])*np.cos(theta[i])
+    
+    yprime1_out = str_out_x_1[i]*(np.sin(theta[i]))**2 + str_out_y_1[i]*(np.cos(theta[i]))**2 - 2*str_out_xy_1[i]*np.sin(theta[i])*np.cos(theta[i])
+    yprime2_out = str_out_x_2[i]*(np.sin(theta[i]))**2 + str_out_y_2[i]*(np.cos(theta[i]))**2 - 2*str_out_xy_2[i]*np.sin(theta[i])*np.cos(theta[i])
+    yprime3_out = str_out_x_3[i]*(np.sin(theta[i]))**2 + str_out_y_3[i]*(np.cos(theta[i]))**2 - 2*str_out_xy_3[i]*np.sin(theta[i])*np.cos(theta[i])
+    yprime4_out = str_out_x_4[i]*(np.sin(theta[i]))**2 + str_out_y_4[i]*(np.cos(theta[i]))**2 - 2*str_out_xy_4[i]*np.sin(theta[i])*np.cos(theta[i])
+    yprime5_out = str_out_x_5[i]*(np.sin(theta[i]))**2 + str_out_y_5[i]*(np.cos(theta[i]))**2 - 2*str_out_xy_5[i]*np.sin(theta[i])*np.cos(theta[i])
+    
+    yprime1_in = str_in_x_1[i]*(np.sin(theta[i]))**2 + str_in_y_1[i]*(np.cos(theta[i]))**2 - 2*str_in_xy_1[i]*np.sin(theta[i])*np.cos(theta[i])
+    yprime2_in = str_in_x_2[i]*(np.sin(theta[i]))**2 + str_in_y_2[i]*(np.cos(theta[i]))**2 - 2*str_in_xy_2[i]*np.sin(theta[i])*np.cos(theta[i])
+    yprime3_in = str_in_x_3[i]*(np.sin(theta[i]))**2 + str_in_y_3[i]*(np.cos(theta[i]))**2 - 2*str_in_xy_3[i]*np.sin(theta[i])*np.cos(theta[i])
+    yprime4_in = str_in_x_4[i]*(np.sin(theta[i]))**2 + str_in_y_4[i]*(np.cos(theta[i]))**2 - 2*str_in_xy_4[i]*np.sin(theta[i])*np.cos(theta[i])
+    yprime5_in = str_in_x_5[i]*(np.sin(theta[i]))**2 + str_in_y_5[i]*(np.cos(theta[i]))**2 - 2*str_in_xy_5[i]*np.sin(theta[i])*np.cos(theta[i])
+    
 
     # append all the values to lists:
     str_xprime1_out.append(xprime1_out)
     str_xprime2_out.append(xprime2_out)
+    str_xprime3_out.append(xprime3_out)
+    str_xprime4_out.append(xprime4_out)
+    str_xprime5_out.append(xprime5_out)
+    
+    str_xprime1_in.append(xprime1_in)
+    str_xprime2_in.append(xprime2_in)
+    str_xprime3_in.append(xprime3_in)
+    str_xprime4_in.append(xprime4_in)
+    str_xprime5_in.append(xprime5_in)
+    
+    str_yprime1_out.append(yprime1_out)
+    str_yprime2_out.append(yprime2_out)
+    str_yprime3_out.append(yprime3_out)
+    str_yprime4_out.append(yprime4_out)
+    str_yprime5_out.append(yprime5_out)
+    
+    str_yprime1_in.append(yprime1_in)
+    str_yprime2_in.append(yprime2_in)
+    str_yprime3_in.append(yprime3_in)
+    str_yprime4_in.append(yprime4_in)
+    str_yprime5_in.append(yprime5_in)
 
 
 ########################################################################################################
 
+'''
+# Get bending strain
+    
+bendstr1 = []
+bendstr2 = []
+bendstr3 = []
+bendstr4 = []
+bendstr5 = []
 
-
-
-
+for j in range(0, len(str_xprime1_out)):
+    str_out_1 = np.sqrt(str_xprime1_out[j]**2 + str_yprime1_out[j]**2 + str_out_z_1[j]**2)
+    str_out_2 = np.sqrt(str_xprime2_out[j]**2 + str_yprime2_out[j]**2 + str_out_z_2[j]**2)
+    str_out_3 = np.sqrt(str_xprime3_out[j]**2 + str_yprime3_out[j]**2 + str_out_z_3[j]**2)
+    str_out_4 = np.sqrt(str_xprime4_out[j]**2 + str_yprime4_out[j]**2 + str_out_z_4[j]**2)
+    str_out_5 = np.sqrt(str_xprime5_out[j]**2 + str_yprime5_out[j]**2 + str_out_z_5[j]**2)
+    
+    str_in_1 = np.sqrt(str_xprime1_in[j]**2 + str_yprime1_in[j]**2 + str_in_z_1[j]**2)
+    str_in_2 = np.sqrt(str_xprime2_in[j]**2 + str_yprime2_in[j]**2 + str_in_z_2[j]**2)
+    str_in_3 = np.sqrt(str_xprime3_in[j]**2 + str_yprime3_in[j]**2 + str_in_z_3[j]**2)
+    str_in_4 = np.sqrt(str_xprime4_in[j]**2 + str_yprime4_in[j]**2 + str_in_z_4[j]**2)
+    str_in_5 = np.sqrt(str_xprime5_in[j]**2 + str_yprime5_in[j]**2 + str_in_z_5[j]**2)
+    
+    str_bend_1 = (str_out_1 - str_in_1) / 2
+    str_bend_2 = (str_out_2 - str_in_2) / 2
+    str_bend_3 = (str_out_3 - str_in_3) / 2
+    str_bend_4 = (str_out_4 - str_in_4) / 2
+    str_bend_5 = (str_out_5 - str_in_5) / 2
+    
+    bendstr1.append(str_bend_1)
+    bendstr2.append(str_bend_2)
+    bendstr3.append(str_bend_3)
+    bendstr4.append(str_bend_4)
+    bendstr5.append(str_bend_5)
+'''
 
 ########################################################################################################
 # Plot strains:
