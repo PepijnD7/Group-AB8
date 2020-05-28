9295a13107778f69cef83da0293216af9db25e92
DaanDieben <62008659+DaanDieben@users.noreply.github.com>
2020-05-28 16:04:56 +0200

    WORK
    Nice graphs

--- FEM data analysis/FEM Force vs. Displacement.py
+++ FEM data analysis/FEM Force vs. Displacement.py
@@ -3,8 +3,8 @@
 from math import *
 import matplotlib.pyplot as plt
 import numpy as np
-
-
+from scipy import interpolate
+from scipy import integrate
 
 # Open Force and Displacement file:
 ForceDisp = open("femForceDisplacement.txt")
@@ -16,16 +16,56 @@ ForceDisp.close()
 array = np.genfromtxt("femForceDisplacement.txt",skip_header=1)
 
 
-# Create lists to plot:
-displacement = array[:,0]   # Displacement
-force = array[:,1]          # Actuation force
+# Create arrays to plot:
+displacement = array[:,0]
+force        = array[:,1]
 
+displacement1 = [array[1,0]]
+displacement2 = [array[2,0]]
+displacement3 = [array[3,0]]
+displacement4 = [array[4,0]]
+force1 = [array[1,1]]
+force2 = [array[2,1]]
+force3 = [array[3,1]]
+force4 = [array[4,1]]
+
+# Create spline to fit a curve through the force-displacement data:
+x_axis = np.linspace(0,39.2,50)  # displacement interval
+spline = interpolate.interp1d(displacement, force, kind="cubic")
+
+
+# Integrate the spline to find the work done:
+work = [] 
+for i in range(len(x_axis)):
+    U = integrate.trapz(spline(x_axis[:i]),x_axis[:i])
+    work.append(U/1000)
+    
+
+print(work)
 
 # Plot force and displacement:
-plt.plot(force, displacement, 'r', linestyle='solid', marker='o')
-plt.xlabel("Actuation force [N]")
-plt.ylabel("Displacement [mm]")
-plt.legend()
-plt.title("Actuation force vs. displacement")
+plt.plot(x_axis, spline(x_axis), 'b')
+#plt.plot(0 , 0 , 'b' , marker='o')
+plt.plot(displacement1, force1, 'b', marker='o')
+plt.plot(displacement2, force2, 'b', marker='o')
+plt.plot(displacement3, force3, 'b', marker='o')
+plt.plot(displacement4, force4, 'b', marker='o')
+plt.ylabel("Actuation force [N]")
+plt.xlabel("Displacement [mm]")
+plt.xlim(0,40)
+plt.ylim(0,1300)
 plt.grid()
 plt.show()
+
+
+# Plot work and displacement:
+plt.plot(x_axis, work, 'b')
+plt.ylabel("Work [J]")
+plt.xlabel("Displacement [mm]")
+plt.grid()
+plt.xlim(0,40)
+plt.ylim(0,30)
+plt.show()
+
+
+print('Done')
